0f2233c51f5b1165b582543019bba88ce25b91c9
VIAUD Nathan <nathan.viaud@inria.fr>
2024-10-09 15:55:52 +0200

    chores(e2e): remove unused variable

--- Front/tests/e2e/simple_test.spec.js
+++ Front/tests/e2e/simple_test.spec.js
@@ -12,5 +12,5 @@ test('should interface with the backend', async ({ request}) => {
     const response = await request.get('http://localhost:3000/config');
 
     expect(response.ok()).toBeTruthy();
-    const responseBody = await response.json();
+    // const responseBody = await response.json();
 })
