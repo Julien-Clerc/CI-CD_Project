e56692719daaa76432b2ca94c6420aa367595063
VIAUD Nathan <nathan.viaud@inria.fr>
2024-10-09 15:23:06 +0200

    chores: test uses preview instead of dev server

--- Front/tests/e2e/simple_test.spec.js
+++ Front/tests/e2e/simple_test.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Simple E2E Test', () => {
     test('check title', async ({ page }) => {
-        await page.goto('http://localhost:5173');
+        await page.goto('http://localhost:4173');
 
         await expect(page.locator('text=Accueil')).toBeVisible()
     })
